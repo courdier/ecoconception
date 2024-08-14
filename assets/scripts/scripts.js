f91180d294c5794297b7f9bbbcccc6e701d12b4f
Lise ROCHAT <lise.rochat@esiroi.re>
2024-08-15 00:55:56 +0400

    bugfix index

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -291,8 +291,8 @@ function showResults() {
         </ul>
         <br>
         <div class="button-container">
-            <button onclick="location.href='../pages/fiche.html'" class="button">Quelques Chiffres</button>
-            <button onclick="location.href='../pages/en-pratique.html'" class="button-secondary">En Pratique</button>
+            <button onclick="location.href='../../pages/quelques-chiffres.html'" class="button">Quelques Chiffres</button>
+            <button onclick="location.href='../../pages/en-pratique.html'" class="button-secondary">En Pratique</button>
         </div>
     `;
 }
